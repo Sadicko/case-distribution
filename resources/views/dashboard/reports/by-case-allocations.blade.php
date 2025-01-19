d98fc00cd47191971b7cd7256d2601a9eb92e20c
Sadicko <kojos28@gmail.com>
2025-01-19 10:55:47 +0000

    Implemented report for case allocations, this list all cases for courts to be printed, resolve issue on updating location

---
+++ resources/views/dashboard/reports/by-case-allocations.blade.php
@@ -0,0 +1,39 @@
+@extends('dashboard.layouts.app')
+
+@section('title', 'Workload  by courts')
+
+@section('report_collapse', 'show')
+@section('reports_active', 'active')
+@section('court_report_active', 'active')
+
+@section('content')
+<div class="body d-flex py-3">
+    <div class="container-xxl">
+        <div class="row align-items-center">
+            <div class="border-0 mb-4">
+                <div class="card-header py-3 no-bg bg-transparent d-flex align-items-center px-0 justify-content-between border-bottom flex-wrap">
+                    <h3 class="h4 mb-0"><i class="fas fa-chart-pie"></i> Workload by courts</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex mb-2">
+            <h6 class="text-muted me-3">LEGAL YEAR:</h6>
+            <small>{{ getCustomLocalDate($legalYearStart) .' - '. getCustomLocalDate($legalYearEnd) }}</small>
+        </div>
+
+    </div>
+</div>
+
+<div class="body d-flex py-3">
+    <div class="container-xxl">
+        <div class="card mb-3">
+            <div class="card-body basic-custome-color">
+
+                @livewire('case-load-by-court', ['legalYearStart' => $legalYearStart, 'legalYearEnd' => $legalYearEnd])
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
